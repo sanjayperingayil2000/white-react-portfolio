4cda9db1d5e2e8548781f327a645d80186238561
Sanjay peringayil <sanjayperingayil@gmail.com>
2025-11-11 14:21:52 +0530

    scroll

--- src/components/scrollup/Scrollup.jsx
+++ src/components/scrollup/Scrollup.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 import './scrollup.css'
 
 const Scrollup = () => {
+    // 1. Logic to show/hide the button
     window.addEventListener("scroll", function () {
-        const scrollUp = this.document.querySelector(".scrollup");
+        const scrollUp = document.querySelector(".scrollup");
         if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
         else scrollUp.classList.remove("show-scroll")
-    })
-  return (
-    <a href="#" className='scrollup'>
-        <i className='uil uil-arrow-up scrollup__icon'></i>
-    </a>
-  )
+    });
+    
+    // 2. Function to handle the click (scroll to top)
+    const scrollUpToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' 
+        });
+    };
+
+    return (
+        <button className='scrollup' onClick={scrollUpToTop}> 
+            <i className='uil uil-arrow-up scrollup__icon'></i>
+        </button>
+    )
 }
 
 export default Scrollup
