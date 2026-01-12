59af3af93dd0ef2963bbf307a82e9ae61ab40c5e
Sanjay peringayil <sanjayperingayil@gmail.com>
2026-01-12 15:25:11 +0530

    experience updated

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -75,6 +75,25 @@ const Qualification = () => {
                 </div>
 
                 <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
+                    
+                    <div className='qualification__data'>
+                    <div></div>
+
+                    <div>
+                        <span className='qualification__rounder'></span>
+                        <span className='qualification__line'></span>
+                    </div>
+
+                        <div>
+                            <h3 className='qualification__title'>Full Stack developer</h3>
+                            <span className='qualification__subtitle'>JAH Transport by heavy & light trucks LLC</span>
+                            <div className='qualification__calendar'>
+                                <i className='uil uil-calendar-alt'></i> 2025 - 2025
+                            </div>
+                        </div>
+
+                    </div>
+                    
                     <div className='qualification__data'>
                         <div>
                             <h3 className='qualification__title'>Junior Software Engineer</h3>
